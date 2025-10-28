7bcc86f9d4b7f6df362abb7d75d529c240708760
“Lukas” <info@brainlesslukas.xyz>
2025-10-29 00:30:04 +0100

    SubCommand Switch/Finder

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
 import hello from "../src/commands/hello.js"
+import addComponent from "../src/commands/addComponent.js"
 
-hello()
+const subCommand = process.argv[2]
+
+switch(subCommand) {
+    case "add":
+        addComponent()
+        break
+    default:
+        console.log("No valid subcommand found.")                    
+}
