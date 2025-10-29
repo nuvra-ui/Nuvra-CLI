8e2b5c0568390174e0e51fdd84fcdc28188cddec
“Lukas” <info@brainlesslukas.xyz>
2025-10-29 01:15:17 +0100

    Create Destination Folder if not found

--- src/commands/addComponent.js
+++ src/commands/addComponent.js
@@ -1,7 +1,15 @@
 import chalk from 'chalk'
+import fs from 'fs'
+import path from 'path'
 
 export default function addComponent() {
     const positionalArgument = process.argv[3] //e.g npx @nuvra-ui/nuvra-ui add <positionalArgument>
+    const rootDir = path.resolve() // e.g /home/joe/my-project
 
     console.log(chalk.yellow(`Searching for ${positionalArgument}-Component...`))
+
+    fs.mkdir(`${rootDir}/src/ui/${positionalArgument}`, { recursive: true }, (err) => {
+        if (err) throw err;
+        console.log("Created Folder.")
+    })
 }
