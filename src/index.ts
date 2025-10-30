94fb1dbb5a291cb38efec70f2dbb29ad21f2a844
“Lukas” <info@brainlesslukas.xyz>
2025-10-30 15:05:03 +0100

    Register Add Command

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Command } from "commander";
 import { hello } from "./commands/hello";
+import { add } from "./commands/add";
 
 const program = new Command()
   .name("nuvra-cli")
   .description("Nuvra-UI Command Line Interface")
   .version("0.1.0", "-v, --version", "output the current version");
 
-program.addCommand(hello);
+program.addCommand(hello).addCommand(add);
 
 program.parse();
