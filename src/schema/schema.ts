27a785a5db4f69087457a44dbd19dbc291873132
“Lukas” <info@brainlesslukas.xyz>
2025-11-04 22:39:14 +0100

    Schema Options

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -2,8 +2,11 @@ import * as z from "zod";
 
 const componentSchema = z.object({
   name: z.string(),
-  author: z.string(),
   description: z.string(),
+  author: z.string(),
+  version: z.string().optional(),
+  tags: z.array(z.string()).optional(),
+  category: z.string().optional(),
 });
 
 export { componentSchema };
