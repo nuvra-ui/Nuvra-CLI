a5713e4ec40638de54a10cfd7a5cda26660c5fe5
“Lukas” <info@brainlesslukas.xyz>
2025-11-04 19:06:50 +0100

    Create Registry Folder with CLI

--- src/commands/registry/build.ts
+++ src/commands/registry/build.ts
@@ -1,13 +1,19 @@
 import { Command } from "commander";
+import fs from "fs";
 
 export const build = new Command()
   .name("build")
   .description("Convert a component file into a JSON file for the registry")
   .argument("[file]", "path to the component file to build")
-  .action(async (file: string) => {
-    await buildRegistryComponent(file);
+  .action(async () => {
+    await buildRegistryComponent();
   });
 
-async function buildRegistryComponent(file: string) {
-  console.log("Building: " + file);
+async function buildRegistryComponent() {
+  if (!fs.existsSync("./registry")) {
+    console.log("Creating registry folder...");
+    fs.mkdirSync("./registry", { recursive: true });
+  } else {
+    console.log("Registry folder already exists.");
+  }
 }
