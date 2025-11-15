fb5d430424d355ae5dae64c73c508e99595815f2
“Lukas” <info@brainlesslukas.xyz>
2025-11-16 00:42:45 +0100

    parse version

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import { hello } from "./commands/hello";
 import { add } from "./commands/add";
 import { build } from "./commands/registry/build";
 
+import { getVersion } from "./utils/getVersion";
+
 const program = new Command()
   .name("nuvra-cli")
   .description("Nuvra-UI Command Line Interface")
-  .version("0.1.0", "-v, --version", "output the current version");
+  .version(getVersion(), "-v, --version", "output the current version");
 
 program.addCommand(hello).addCommand(add).addCommand(build);
 
