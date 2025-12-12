a65bfb391c45ef8272d785c65f0384a7f446950f
Lukas <info@brainlesslukas.xyz>
2025-12-12 20:45:33 +0100

    colored hello command

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,8 +1,9 @@
+import chalk from "chalk";
 import { Command } from "commander";
 
 export const hello = new Command()
   .name("hello")
   .description("Prints a hello message from Nuvra-UI (testing)")
   .action(async () => {
-    console.log("Hello from Nuvra-UI!");
+    console.log(chalk.green("Hello from Nuvra-UI!"));
   });
