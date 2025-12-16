30b0b6cc938bb96d71b8d729e175fed7979d24f2
Lukas <info@brainlesslukas.xyz>
2025-12-16 20:27:59 +0100

    refacor add cmd

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -4,6 +4,7 @@ import chalk from "chalk";
 import { getRegistry } from "../utils/getRegistry";
 import { getMetadata } from "../utils/getMetadata";
 import { getComponentFile } from "../utils/getComponentFile";
+import { getData } from "../utils/getData";
 
 export const add = new Command()
   .name("add")
@@ -14,13 +15,13 @@ export const add = new Command()
   });
 
 async function addComponent(component: string) {
-  const registry = await getRegistry();
+  const registry = await getData("/src/registry.json");
 
   if (registry[component]) {
-    const metaData = await getMetadata(registry[component].path);
+    const metaData = await getData(registry[component].path + "/metadata.json");
     console.log(metaData);
 
-    const componentFile = await getComponentFile(metaData.files[0].path);
+    const componentFile = await getData("/" + metaData.files[0].path);
 
     if (!existsSync(`src/ui/`)) {
       mkdirSync(`src/ui/`);
