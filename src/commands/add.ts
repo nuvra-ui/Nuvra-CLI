01d4b8e5d2ddb4acd233bc1f88259d32dfb5d19c
Lukas <info@brainlesslukas.xyz>
2025-12-30 18:12:28 +0100

    impl: framework selection

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,5 +1,5 @@
 import { Command } from "commander";
-import { multiselect } from "@clack/prompts";
+import { multiselect, select } from "@clack/prompts";
 
 export const add = new Command()
   .name("add")
@@ -7,13 +7,20 @@ export const add = new Command()
   .action(addComponent)
 
 async function addComponent() {
-  const componentSelection = await multiselect({
+  const framework = await select({
+    message: "Pick the framework of your project.",
+    options: [
+      { value: "react", label: "React (tsx)" },
+      { value: "vue", label: "Vue.js" },
+    ]
+  });
+  const component = await multiselect({
     message: "Select the components you want to add to your project.",
     options: [
       { value: "button", label: "Button" },
       { value: "link", label: "Link" },
       { value: "switch", label: "Switch" },
     ],
-    required: true
+    required: true,
   });
 }
