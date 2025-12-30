b04507a449520d4b967a2c8f9a24bbb16772cf2e
Lukas <info@brainlesslukas.xyz>
2025-12-30 15:41:44 +0100

    impl: commander CLI setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-console.log("Hello World");
- 
+import { Command } from "commander";
+import packageJson from "../package.json" with { type: "json" }
+
+async function main() {
+  const program = new Command()
+    .name("Nuvra-CLI")
+    .description("The CLI for the all in one UI-Library")
+    .version(packageJson.version)
+
+    program.parse()
+}
+
+main()
