8ed629067727e60a0f24dbcd58dcd61482134249
Lukas <info@brainlesslukas.xyz>
2025-12-31 01:02:22 +0100

    feat: log selected registry data

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,5 +1,6 @@
 import { Command } from "commander";
-import { multiselect, select } from "@clack/prompts";
+import { multiselect, select, isCancel, cancel } from "@clack/prompts";
+import { getFile } from "../utils/getFile.js";
 
 export const add = new Command()
   .name("add")
@@ -11,19 +12,34 @@ async function addComponent() {
     message: "Pick the framework of your project.",
     options: [
       { value: "react", label: "React (tsx)" },
-      { value: "vue", label: "Vue.js" },
+      { value: "vuejs", label: "Vue.js" },
     ],
   });
+  if (isCancel(framework)) {
+    cancel("Operation cancelled.");
+    process.exit(0);
+  }
+
   const component = await multiselect({
     message: "Select the components you want to add to your project.",
     options: [
-      { value: "button", label: "Button" },
-      { value: "link", label: "Link" },
-      { value: "switch", label: "Switch" },
+      { value: "Button", label: "Button" },
+      { value: "Link", label: "Link" },
+      { value: "Switch", label: "Switch" },
     ],
     required: true,
   });
+  if (isCancel(component)) {
+    cancel('Operation cancelled.')
+    process.exit(0);
+  }
 
-  const registry = getFi
+  const registry = await getFile("/registry.json");
 
+  component.forEach(name => {
+    const content = registry[framework].find(
+      (item: { name: string }) => item.name === name,
+    );
+    console.log(content)
+  })
 }
