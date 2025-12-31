6da9e97f1605ce5977d19fb2b8c920261de11228
Lukas <info@brainlesslukas.xyz>
2025-12-31 01:29:49 +0100

    refactor: grouping prompts

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,5 +1,5 @@
 import { Command } from "commander";
-import { multiselect, select, isCancel, cancel } from "@clack/prompts";
+import * as p from "@clack/prompts";
 import { getFile } from "../utils/getFile.js";
 
 export const add = new Command()
@@ -8,38 +8,37 @@ export const add = new Command()
   .action(addComponent);
 
 async function addComponent() {
-  const framework = await select({
-    message: "Pick the framework of your project.",
-    options: [
-      { value: "react", label: "React (tsx)" },
-      { value: "vuejs", label: "Vue.js" },
-    ],
-  });
-  if (isCancel(framework)) {
-    cancel("Operation cancelled.");
-    process.exit(0);
-  }
+  const group = await p.group(
+    {
+      framework: () =>
+        p.select({
+          message: "Pick the framework of your project.",
+          options: [
+            { value: "react", label: "React (tsx)" },
+            { value: "vuejs", label: "Vue.js" },
+          ],
+        }),
+      component: () =>
+        p.multiselect({
+          message: "Select the components you want to add to your project.",
+          options: [
+            { value: "Button", label: "Button" },
+            { value: "Link", label: "Link" },
+            { value: "Switch", label: "Switch" },
+          ],
+          required: true,
+        }),
+    },
+    {
+      onCancel: () => {
+        p.cancel("Operation cancelled.");
+        process.exit(0);
+      },
+    },
+  );
 
-  const component = await multiselect({
-    message: "Select the components you want to add to your project.",
-    options: [
-      { value: "Button", label: "Button" },
-      { value: "Link", label: "Link" },
-      { value: "Switch", label: "Switch" },
-    ],
-    required: true,
-  });
-  if (isCancel(component)) {
-    cancel('Operation cancelled.')
-    process.exit(0);
-  }
+  console.log(group.framework, group.component);
 
   const registry = await getFile("/registry.json");
-
-  component.forEach(name => {
-    const content = registry[framework].find(
-      (item: { name: string }) => item.name === name,
-    );
-    console.log(content)
-  })
+  console.log(registry);
 }
