1ca65c10b468042bb5e0e1c53dfb58d9a85d2137
Lukas <info@brainlesslukas.xyz>
2026-01-01 16:53:24 +0100

    fix: schema

--- src/schemas/registrySchema.ts
+++ src/schemas/registrySchema.ts
@@ -1,28 +1,17 @@
 import * as z from "zod";
 
+const registryItemSchema = z.object({
+  name: z.string(),
+  author: z.string(),
+  dependencies: z.array(z.string()),
+  files: z.array(
+    z.object({
+      path: z.string(),
+    }),
+  ),
+});
+
 export const registrySchema = z.object({
-  vuejs: z.array(
-    z.object({
-      name: z.string(),
-      author: z.string(),
-      dependencies: z.array(z.string()),
-      files: z.array(
-        z.object({
-          path: z.string(),
-        }),
-      ),
-    }),
-  ),
-  react: z.array(
-    z.object({
-      name: z.string(),
-      author: z.string(),
-      dependencies: z.array(z.string()),
-      files: z.array(
-        z.object({
-          path: z.string(),
-        }),
-      ),
-    }),
-  ),
+  vuejs: z.array(registryItemSchema).default([]),
+  react: z.array(registryItemSchema).default([]),
 });
