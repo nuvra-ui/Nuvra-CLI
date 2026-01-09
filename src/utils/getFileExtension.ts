e595cebd9b1dc844d4751efab4b3af43f3e12f85
Lukas <info@brainlesslukas.xyz>
2026-01-09 17:56:35 +0100

    feat: getFileExtension util

---
+++ src/utils/getFileExtension.ts
@@ -0,0 +1,10 @@
+export function getFileExtension(framework: string): string {
+  switch(framework) {
+    case "react":
+      return ".tsx"
+    case "vuejs":
+      return ".vue"
+    default:
+      return ".js"
+  }
+}
